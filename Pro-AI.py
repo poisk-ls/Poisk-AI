093c377c7d9a3a6c94bca4400a02f9fd84f30d2a
checkmate <112060601+poisk-ls@users.noreply.github.com>
2022-10-24 01:18:54 +0800

    Add files via upload

---
+++ Pro-AI.py
@@ -0,0 +1,22 @@
+import time
+import datetime
+import os
+import subprocess
+
+
+
+os.system('screenfetch')
+print('\033[1m' + '\033[91m'+'     ///   customize by poisk-ls   ///')
+def wish():
+  h=int(datetime.datetime.now().hour)
+  if h<12:
+     subprocess.call(["termux-tts-speak","Good morning pro","how may i help you"])
+  else:
+     if h>=12 and h<17:
+       subprocess.call(["termux-tts-speak","good afternoon pro","how may i help you"])
+     elif h>=17 and h<20:
+       subprocess.call(["termux-tts-speak","good evening pro,how may i help you"])
+     else:
+       subprocess.call(["termux-tts-speak","welcome pro how may i help you"])
+wish()
+os.system("python main.py")
